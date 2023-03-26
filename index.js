b219998cabe1fce2507f664aa438197315db949b
santaNan <salazarmiguelito23@gmail.com>
2023-03-26 16:24:21 -0500

    eight2

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const { Client, RemoteAuth } = require('whatsapp-web.js');
 const express = require('express');
 const qrcode = require('qrcode-terminal');
-const bodyParse = require('body-parser')
+const bodyParse= require('body-parser')
 const cors = require('cors')
-
 const app = express()
 
 
@@ -30,13 +29,11 @@ mongoose.connect(URL).then(() => {
         authStrategy: new RemoteAuth({
             dataPath: '/tmp',
             store: store,
-            backupSyncIntervalMs: 300000,
+            backupSyncIntervalMs: 10000000,
         }),
-        puppeteer: { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--single-process', '--disable-gpu',] }
+        puppeteer: { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
     });
     console.log('hola22');
-    client.initialize();
-
     client.on('qr', (qr) => {
         // NOTE: This event will not be fired if a session is specified.
         qrcode.generate(qr, { small: true });
@@ -46,23 +43,20 @@ mongoose.connect(URL).then(() => {
         console.log('SAVED');
     })
     console.log('hola3');
+    console.log('hola4');
 
     client.on('ready', () => {
-        console.log('ready');
-       
         // Do Stuff...
+        // client.sendMessage(`573143551942@c.us`, 'Hola')
+        console.log('ready');
     })
-    app.post("/sendmessage/vendedor/", (req, res) => {
+    app.post("/hi", (req, res) => {
         console.log(req.body);
-        if (client.info === undefined) {
-            console.log('el sistema aún no está listo');
-        }else{
-            client.sendMessage(`573143551942@c.us`, 'Que mas23')
-            res.send('enviado correctamente').status(204)
-    
-        }
-        
+
+        client.sendMessage(`${req.body.number}@c.us`, req.body.titulo)
+        res.send('enviado correctamente').status(204)
     })
+    client.initialize();
 
 });
 
